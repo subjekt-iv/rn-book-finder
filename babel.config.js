46ee6774dedc807e5bcd64e286764e45e9df8c66
subjekt-iv <agustinprado007@gmail.com>
2024-05-17 17:08:11 -0300

    added config for absolute paths and implemented atomic design patter / folder structure

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['nativewind/babel'],
+  plugins: [
+    'nativewind/babel',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
