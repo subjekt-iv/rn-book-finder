41ff1cc25f1dbf5bb1d40acf8cba40e5db211bdd
subjekt-iv <agustinprado007@gmail.com>
2024-05-20 03:55:40 -0300

    added icons, renamed apps, created splash screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import {createStackNavigator} from '@react-navigation/stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -6,10 +6,17 @@ import BookListScreen from '@/screens/book-list-screen';
 import BookDetailScreen from '@/screens/book-detail-screen';
 import BookRecentScreen from '@/screens/book-recent-screen';
 import {NavigationContainer, DarkTheme} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
+import {Platform} from 'react-native';
 
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
   return (
     <SafeAreaProvider>
       <NavigationContainer theme={DarkTheme}>
